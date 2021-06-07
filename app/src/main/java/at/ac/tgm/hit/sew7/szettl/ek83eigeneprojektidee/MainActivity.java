2dffcf3a93477504cd7e8c3e78a9c1132519f92c
szettl135 <szettl@student.tgm.ac.at>
2021-06-07 13:35:14 +0200

    Initial Commit

---
+++ app/src/main/java/at/ac/tgm/hit/sew7/szettl/ek83eigeneprojektidee/MainActivity.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.hit.sew7.szettl.ek83eigeneprojektidee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
